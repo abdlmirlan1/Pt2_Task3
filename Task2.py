c85624d60c4c11b349b2cf5d6df7d3ca5225d4ac
abdlmirlan1 <51496180+abdlmirlan1@users.noreply.github.com>
2019-06-12 12:33:44 +0600

    Delete Task2.py

--- Task2.py
+++
@@ -1,5 +0,0 @@
-def distribute(students,apples):
-    each = apples//students
-    remain = apples%students
-    print('Each student will take '+str(each)+' apples, and remains '+str(remain))
-distribute(6,31)
