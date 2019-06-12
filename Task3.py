120048f9b4d5d69f5e7e32f07af74f590d76cc0f
Mirlan Abdulakhatov <abdlmirlan1@gmail.com>
2019-06-12 11:35:16 +0600

    Pt2 Task3

---
+++ Task3.py
@@ -0,0 +1,5 @@
+def prevnext(a):
+    previous = a - 1
+    next = a + 1
+    print('The next number for the number 179 is ' + str(next)+'. The previous number for the number 179 is ' + str(previous))
+prevnext(179)
